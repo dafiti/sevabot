6babe502049763724b3493e5fe1d1283de1bfc43
georg.buske@dafiti.com.br <notificator@DFT-SP-DEV01.dafiti.local>
2014-09-21 13:08:11 -0300

    Revert "hopefully fix the commands after idle"
    This reverts commit 9e384699b769b865d40982646ffbe56a40369f88, reversing
    changes made to 6a69d9b1343ff85a3de35a494314896177a0c40e.

--- sevabot/bot/bot.py
+++ sevabot/bot/bot.py
@@ -11,13 +11,9 @@ try:
 except ImportError:
     # python 2.6 or earlier, use backport
     from ordereddict import OrderedDict
-
-from datetime import datetime
-from collections import OrderedDict
 from inspect import getmembers, isclass
 
 import Skype4Py
-from Skype4Py.utils import chop
 
 from sevabot.bot import handlers
 from sevabot.utils import get_chat_id
@@ -40,7 +36,7 @@ class Sevabot:
     def start(self):
 
         if sys.platform == "linux2":
-            self.skype = Skype4Py.Skype(Transport='dbus')
+            self.skype = Skype4Py.Skype(Transport='x11')
         else:
             # OSX
             self.skype = Skype4Py.Skype()
@@ -49,7 +45,7 @@ class Sevabot:
         self.skype.Attach()
 
         logger.debug("Skype API connection established")
-        self.skype.RegisterEventHandler('Notify', self.handleMessages)
+        self.skype.OnMessageStatus = self.handleMessages
 
         self.cacheChats()
 
@@ -92,9 +88,7 @@ class Sevabot:
         for chat in self.skype.Chats:
 
             # filter chats older than 6 months
-            timestamp = chat.ActivityTimestamp
-            if time.time() - timestamp > 3600 * 24 * 180:
-                logger.debug("chat %s last activity %s" % (chat.FriendlyName, datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')))
+            if time.time() - chat.ActivityTimestamp > 3600 * 24 * 180:
                 continue
 
             chats.append(chat)
@@ -104,10 +98,7 @@ class Sevabot:
         for chat in chats:
             # Encode ids in b64 so they are easier to pass in URLs
             chat_id = get_chat_id(chat)
-            logger.debug("chat %s = %s" % (chat.FriendlyName, chat_id))
             self.chats[chat_id] = chat
-   
-        logger.debug("Async cacheChats() completed")
 
     def getOpenChats(self):
         """
@@ -119,27 +110,15 @@ class Sevabot:
         for chat_id, chat in self.chats.items():
             yield chat_id, chat
 
-    def handleMessages(self, notification):
+    def handleMessages(self, msg, status):
         """
         Handle incoming messages.
         """
 
-        logger.debug(notification)
-        a, b = chop(notification)
-        if a == 'CHAT':
-            object_type, object_id, prop_name, value = [a] + chop(b, 2)
-            skype = self.getSkype()
-            if prop_name == 'ACTIVITY_TIMESTAMP':
-                for message in skype.MissedMessages:
-                    logger.debug('SEEN: %s', message.Body)
-                    message.MarkAsSeen()
-                    self.handler.handle(message, 'RECEIVED')
+        logger.debug("Incoming %s - %s - %s: %s" % (status, msg.Chat.FriendlyName,
+                                                    msg.FromHandle, msg.Body))
 
-        #logger.debug("Incoming %s - %s - %s: %s" % (status, msg.Chat.FriendlyName,
-        #                                            msg.FromHandle, msg.Body))
-
-        #msg.MarkAsSeen()
-        #self.handler.handle(msg, status)
+        self.handler.handle(msg, status)
 
     def sendMessage(self, chat, msg):
         """
